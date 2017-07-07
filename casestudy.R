1a66ec906d87e45e443875dcfa60e621c8146f11
Vincent Goulet <vincent.goulet@act.ulaval.ca>
2017-07-07 02:37:18 +0200

    case stufy commented

--- casestudy.R
+++ casestudy.R
@@ -1,90 +1,174 @@
+## Emacs: -*- coding: utf-8; fill-column: 62; comment-column: 27; -*-
+##
+## Copyright (C) 2017 Vincent Goulet
+##
+## This file is part of the project "Computational actuarial
+## science with R - IME 2017 Workshop"
+## http://github.com/vigou3/ime-2017-workshop-computational-actuarial-science-r
+##
+## This work is licensed under a Creative Commons
+## Attribution-ShareAlike 4.0 International License.
+## http://creativecommons.org/licenses/by-sa/4.0/
+
+###
+### REQUIRED PACKAGES
+###
+
+## Package actuar provides functions for zero truncated
+## discrete distribution, for simulation of compound models
+## and for simulation of discrete mixtures.
 library(actuar)
+
+## Package MASS provides function 'fitdistr' to estimate
+## parameters using maximum likelihood.
 library(MASS)
 
+###
+### POISSON PARAMETERS FOR THE THREE TYPES OF PERIL
+###
+
+## We skip modeling of those and just use the following
+## values (they are sensible).
 lambda.A <- 0.008
 lambda.F <- 0.0003
 lambda.W <- 0.005
 
+
+###
+### MODELING OF THE NUMBER OF ITEMS IN A COLLECTION
+### AND THE MARKET VALUE OF ITEMS
+###
+
+### Import the data of a prior inventory.
+
+## The data set is structured as follows: there is one line
+## per item for the collections of all the clients. The
+## variable in the data set are the following:
+##
+##   ID: unique id for a client [string]
+##   Value: market value of an item [numeric]
+##   DateEntry: date of entry of the item in the collection
+##              [string in format YYYY-MM-DD]
+##   DateExit: date of exit from the collection; NA if still
+##             present
+##
+## We use function 'read.csv' to read in the data. We also
+## specify the class of each column because R does not always
+## correctly the correct type we will need.
 inventory <- read.csv("Inventory.csv",
                       colClasses = c(ID = "factor",
                                      Value = "numeric",
                                      DateEntry = "Date",
                                      DateExit = "Date"))
+
+## First summary of our data set. Note the missing values. We
+## already have an idea of the sizes of the largest
+## collections.
 summary(inventory)
 
-## Garder seulement les bouteilles faisant toujours partie de
-## l'inventaire (c.-à-d. ayant DateHeureSortie == NA).
+### Clean up of the data
+
+## We will only keep the items that are still in the
+## inventory, that is those that have DateExit NA.
+##
+## We put our work data set in another object so that we do
+## not have to import again the data set if (when) we mess up.
 data <- subset(inventory, is.na(DateExit))
 
-## Garder seulement le numéro du client et la valeur marchande
-## de la bouteille.
+## We will no longer need the dates of entry and exit.
 data <- subset(data, select = c(ID, Value))
 
-## Il y a environ 4 % de valeurs marchandes manquantes. On
-## les élimine.
-sum(is.na(data$Value))/nrow(data)
+## There are missing market values (about 4%). We drop them.
 data <- droplevels(data[complete.cases(data), ])
 data <- droplevels(data[!is.na(data$Value), ]) # same (here)
 
+## New summary of our simplified data set. No more missing
+## values.
 summary(data)
 
-n <- table(data$ID)  # nombre de bouteilles par cave
-plot(n)              # nombre de bouteilles par cave
+## The market values are very heavy tailed. Here's a graphic
+## of the empirical distribution function of the market
+## values.
+plot(ecdf(data$Value))
 
-
-## On ne conserve que les bouteilles d'une valeur marchande de
-## 1000$ et moins. On ne peut supprimer
-## entièrement les clients qui ont au moins une bouteille de
-## plus de 1000$ car cela en toucherait un trop grand nombre.
+## For modeling purposes, we will keep only the items with a
+## market value of $1,000 and below.
 data <- subset(data, Value <= 1000)
 
-## Valeurs marchandes individuelles.
-mv <- data$Value           # ~175 000 données
-names(mv) <- data$ID
+## To simplify future manipulations, we store frequently use
+## data in new objects.
+mv <- data$Value         # individual market values (~150,000)
+names(mv) <- data$ID     # label data with client ID
+nb <- by(mv, data$ID, length)  # number of items per collection
+mv.tot <- by(mv, data$ID, sum) # total value per collection
 
-## Nombre de bouteilles par client.
-nb <- by(mv, data$ID, length)  # ~250 données
-
-## Valeurs totales des caves.
-mv.tot <- by(mv, data$ID, sum) # ~250 données
-
-
+## Closer inspection of the collection values.
+summary(mv.tot)
 plot(ecdf(mv.tot))
 
-## Keep only cases with a total value between 5000 and 25,000.
-names(mv.tot[mv.tot > 5000 & mv.tot < 25000])
-
+## We will do the rest of the analysis for the range of total
+## market value between $5,000 and $25,000. We drop the other
+## data points.
 mv <- mv[names(mv) %in% names(mv.tot[mv.tot > 5000 & mv.tot < 25000])]
-nb <- by(mv, data$ID, length)  # ~250 données
-mv.tot <- by(mv, names(mv), sum) # ~250 données
+nb <- by(mv, names(mv), length)  # reset with new data
+mv.tot <- by(mv, names(mv), sum) # idem
 
-hist(nb, prob = TRUE)
+## This is the distribution of the total collection values we
+## will be working with.
+summary(mv.tot)
 plot(ecdf(mv.tot))
 
-## Set parameter for peril: accident
+### Modeling
+
+## For the number of affected items by peril accident, we will
+## use a zero-truncated Poisson distribution with parameter
+## lambda set by judgment (!) to 7.
 par.A <- 7
 
-## Estimation for peril: fire
+## The number of items affected by perils fire and water
+## depend on the number of items in a collection.
+##
+## This is quick look at the distribution of this quantity in
+## our data set.
+summary(nb)
+hist(nb)
+
+## We will use zero-truncated negative binomials to model the
+## number of items affected by perils fire and water.
+##
+## First, estimation for peril fire by maximum likelihood. We
+## use estimators for a negative binomial as starting values
+## for the optimization carried by 'fitdistr'.
+##
+## Note: 'fitdistr' returns estimates for the size parameter
+## of the negative binomial and for the mean equal to r(1 -
+## p)/p (where r is the size and p the probability of
+## success).
 start <- fitdistr(nb, "negative binomial")$estimate
-r <- start[1]
-p <- 1/(1 + start[2]/start[1])
+r <- start[1]                  # estimate of size
+p <- 1/(1 + start[2]/start[1]) # conversion mean -> p
+
+## Estimation of the parameters of the zero-truncated negative
+## binomial. Function 'dztnbinom' comes from actuar.
 (par.F <- fitdistr(nb, dztnbinom,
                    start = list(size = r, prob = p))$estimate)
 
+## Visual assessment of the fit.
 hist(nb, prob = TRUE)
 x <- seq(min(nb), max(nb), by = 10)
 points(x, dztnbinom(x, par.F[1], par.F[2]),
        col = "orange", pch = 19)
 
-
-## Estimation for peril: water damage
-nb.W <- round(nb/4)
+## We repeat the same procedure for peril water. Here, we need
+## a model for 25% of the items in a collection.
+nb.W <- round(nb/4)      # support of distribution is integers
 start <- fitdistr(nb.W, "negative binomial")$estimate
 r <- start[1]
 p <- 1/(1 + start[2]/start[1])
 (par.W <- fitdistr(nb.W, dztnbinom,
                    start = list(size = r, prob = p))$estimate)
 
+## Visual assessment of the fit.
 hist(nb.W, prob = TRUE)
 hist(nb.W, breaks = c(0, 10, 20, 30, 40, 50, 60, 80, 100, 120))
 x <- seq(min(nb.W), max(nb.W), by = 10)
@@ -92,43 +176,47 @@ points(x, dztnbinom(x, par.W[1], par.W[2]),
        col = "orange", pch = 19)
 
 ### Modeling of market values
+
+## Quick look at the distribution. Still very heavy tailed
+## even if we kept only the items with a value <= $1,000.
 summary(mv)
 hist(mv)
 hist(mv[mv < 500])
 hist(mv[mv < 500],
      breaks = c(0, 20, 50, 70, 100, 150, 200, 280, 400, 500))
 
+## We skip detail here, but further analysis showed that a
+## model fitted on values smaller than $500 worked best for
+## our application. We use a lognormal distribution.
 (par.mv <- fitdistr(mv[mv <= 500], "lognormal")$estimate)
 
-
 ###
-### Simulation
+### SIMULATION
 ###
 
+## Parameters for the simulation.
+nb.sim <- 10000            # number of simulations
+nb.clients <- 100          # number of clients
 
-
-model.A <- substitute(
-                      list(mu = par.mv[1],
-                           s = par.mv[2]))
-model.F <- substitute(rcompound(rztnbinom(r, p), rlnorm(mu, s)),
-                      list(r = par.F[1],
-                           p = par.F[2],
-                           mu = par.mv[1],
-                           s = par.mv[2]))
-model.W <- substitute(rcompound(rztnbinom(r, p), rlnorm(mu, s)),
-                      list(r = par.W[1],
-                           p = par.W[2],
-                           mu = par.mv[1],
-                           s = par.mv[2]))
-
-## Number of simulations
-nb.sim <- 1000
-
-expr <- substitute(
-    rmixture(n, c(lambda.A, lambda.F, lambda.W),
-             expression(A = rcompound(rztpois(7), rlnorm(mu, s)),
-                        F = rcompound(rztnbinom(r.F, p.F), rlnorm(mu, s)),
-                        W = rcompound(rztnbinom(r.W, p.W), rlnorm(mu, s)))),
+## We first write an expression to simulate 'nb.sim' variates
+## from:
+##
+## 1. a compound Poisson distribution with Poisson parameter
+##    equal to the sum of lambda.A, lambda.F and lambda.W;
+## 2. with claim amounts distributed as a discrete mixture of
+##    - one compound zero-truncated Poisson distribution, and
+##    - two compound zero-truncated negative binomial distributions;
+## 3. withe item values lognormal distributed.
+##
+## We use 'substitute' to "compute on the language" and insert
+## the parameter values in the correct places. Yeah, it is a
+## mouthful.
+call <- substitute(
+    rcomppois(n, sum(c(lambda.A, lambda.F, lambda.W)),
+              rmixture(c(lambda.A, lambda.F, lambda.W),
+                       expression(A = rcompound(rztpois(7), rlnorm(mu, s)),
+                                  F = rcompound(rztnbinom(r.F, p.F), rlnorm(mu, s)),
+                                  W = rcompound(rztnbinom(r.W, p.W), rlnorm(mu, s))))),
     list(n = nb.sim,
          r.F = par.F[1],
          p.F = par.F[2],
@@ -137,18 +225,24 @@ expr <- substitute(
          mu = par.mv[1],
          s = par.mv[2]))
 
-## Number of clients
-nb.clients <- 100
+## The result is a function call ready to be evaluated.
+call
 
+## Running the simulation amounts to evaluate our above call
+## 'nb.clients' times. This is a job for 'replicate'.
+##
+## The results are 'nb.simul' simulated total claim amounts
+## for each of 'nb.clients' clients.
 Sk <- replicate(nb.clients, eval(expr))
 
+## Average total claim amount in the portfolio.
 Wk <- rowSums(Sk)/nb.clients
 
-## Pure premium
+## Pure premium.
 mean(Wk)
 
-## Premium with safety loading
+## Premium with safety loading.
 level <- 0.9
-(VaR.Wk <- quantile(Wk, level))         # VaR(W)
-w <- which(Wk > VaR.Wk)                 # values Wk > var
-(TVaR.Wk <- mean(Wk[w]))                # TVaR(Wk)
+(VaR.Wk <- quantile(Wk, level)) # VaR(W)
+w <- which(Wk > VaR.Wk)         # values Wk > var
+(TVaR.Wk <- mean(Wk[w]))        # TVaR(Wk)
